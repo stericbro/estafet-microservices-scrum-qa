d7bcf9fae9981f48165b78c9dd84e66396178b9a
Steve Brown <steve.brown@estafet.com>
2019-08-30 18:09:22 +0100

    Git repo url for for fork

--- qa-pipeline.groovy
+++ qa-pipeline.groovy
@@ -1,7 +1,7 @@
 node('maven') {
 
 	stage("checkout") {
-		git branch: "master", url: "https://github.com/Estafet-LTD/estafet-microservices-scrum-qa"
+		git branch: "master", url: "https://github.com/stericbro/estafet-microservices-scrum-qa"
 	}
 
 	stage("unit tests") {
